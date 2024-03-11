54e34dcb3a385b5642f5dc8bfdcf02196643a94f
bingisuresh <162981453+bingisuresh@users.noreply.github.com>
2024-03-11 15:57:43 +0530

    Add files via upload

---
+++ file.1py.py
@@ -0,0 +1,22 @@
+print("hello world")
+"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
+---->
+1.)commenting is used for making documentation in a python life
+2.)when the line is commented, that line will not be considered
+as a part of the code
+3.)when the peogram is excuted, commented line will not be in the part
+of excution.
+
+---->
+"""
+
+#import this
+#import is a keyword which is used to communicated between the python files
+
+import antigravity
+
