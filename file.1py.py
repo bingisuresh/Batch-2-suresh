cc567eb24c72cd37e734e2fb3b59f53760784e3c
bingisuresh <162981453+bingisuresh@users.noreply.github.com>
2024-03-11 16:21:09 +0530

    day 1

--- file.1py.py
+++ file.1py.py
@@ -1,5 +1,65 @@
-print("hello world")
-"""
+
+'''
+Bargained characters
+ controls and Data structures--->ABC language
+ strings and its formats --> from c
+ Regular expressions--> pearl
+ Exception handling--> module 3
+ Magic methods from -->forton
+
+ what kind of language is python?
+
+python is a
+
+    object oriented programing language
+    dynamically typed language
+    scripting language
+    interepted language
+    compiled language
+
+what are the flavours of python
+
+ > Cpython- written in c, most common implementation of python
+ > Jython - java implementation of python
+ > Ironpython- implementation of python programming language which is tightly integrated with NET.
+ > Brytho- browser python, runs in browsers
+ > Rubypython- bridge between python and ruby interpreters
+ > Micropython- runs in micro controllers
+
+ #---> print(object, sep, end, file, flush)
+ #print has 5 parameters
+
+ # object --> the value to be printed is object
+ # sep--> seperator
+ # eg
+ # a=8
+ # b=9
+ # c=89not keep the command to stay in a same line
+ 
+ # print(a,b,c sep='$'
+
+ # end--> which is used to dd the string at the end of the value,
+ # # it will not keep the command to stay in a same line
+ # eg:
+ # print(str(a),b, end='%%')
+ # print(str(c))
+ 
+ # ! ----> variables
+ # ?variables is a name which is used to identify the value of variable
+ # ?which is stored in a specific memory location
+
+# ! Rules to define variables
+
+# 1)variable have to start with only alphabets, underscores
+# 2)the char following the 1st char can be alphabets, numbers, underscores
+# 3)no spaces are allowded
+# 4)no specioal charcters like !@#$%^&*()+
+# 5)case sensitive
+# 6) keywords are not allowed as variables
+ 
+
+'''
+ print("hello world")
 print("hello world")
 print("hello world")
 print("hello world")
